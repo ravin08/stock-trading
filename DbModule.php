d142e8ce00c7dfd69730eef6d8bbedcd011d6dca
ravin08 <62800353+ravin08@users.noreply.github.com>
2022-07-27 18:15:19 +0530

    update file

--- DbModule.php
+++ DbModule.php
@@ -3,13 +3,15 @@ require "config.php";
 
 /**
  * mysql- PDO Class
+ * @version 0.0.1
+ * TODO: test
  */
 
 class mySqlDb{
     
-   public $conn; 
+   private $conn; 
 
-    public function connect_to_db_server(){
+    private function connect_to_db_server(){
         // Create connection PDO
         $database = DATABASE;
         $server_name = SERVER_NAME;
@@ -64,14 +66,93 @@ class mySqlDb{
     }
 
     public function create_table($qry){
-
+        $this->qry = $qry; // FIXME: uncomplite method/ work is pending
+        $conn=$this->  connect_to_db_server(); // call method from same class
+        // $qry = "SHOW DATABASES";
+        $prepare = $conn->prepare($this->qry);
+        $prepare-> execute();
+        $result =$prepare-> fetchAll(pdo::FETCH_ASSOC); // todo work pending
+        return $result;
         
     }
 
    
+    public function fetch_table_data($qry){
+        $this->qry = $qry;
+        $conn=$this->  connect_to_db_server(); // call method from same class
+        // $qry = "SHOW DATABASES";
+        $prepare = $conn->prepare($this->qry);
+        $prepare-> execute();
+        $result =$prepare-> fetchAll(pdo::FETCH_ASSOC);
+        return $result;
 
+    }
 
-    
+    public function insert_table_data($qry){
+        $this->qry = $qry;
+        $conn=$this->  connect_to_db_server(); // call method from same class
+        // $qry = "SHOW DATABASES";
+        $prepare = $conn->prepare($this->qry);
+        $result= $prepare-> execute();
+        if($result){
+            echo "Data Sucessfuly inserted";
+        }else{
+            echo "Data not inserted.";
+        }
+
+        return $result;
+
+    }
+
+    public function update_table_data ($qry){
+        $this->qry = $qry;
+        $conn=$this->  connect_to_db_server(); // call method from same class
+        // $qry = "SHOW DATABASES";
+        $prepare = $conn->prepare($this->qry);
+        $result= $prepare-> execute();
+        if($result){
+            echo "Data update Sucessfuly";
+        }else{
+            echo "Data not updated.";
+        }
+
+        return $result;
+
+    }
+
+    public function delete_table_data($qry){
+        $this->qry = $qry;
+        $conn=$this->  connect_to_db_server(); // call method from same class
+        // $qry = "SHOW DATABASES";
+        $prepare = $conn->prepare($this->qry);
+        $result= $prepare-> execute();
+        
+        if($result){
+            echo "Table data deleted";
+        }else{
+            echo "Table data not deleted.";
+        }
+
+        return $result;
+
+    }
+
+    public function alter_table($qry){
+        $this->qry = $qry;
+        $conn=$this->  connect_to_db_server(); // call method from same class
+        // $qry = "SHOW DATABASES";
+        $prepare = $conn->prepare($this->qry);
+        $result= $prepare-> execute();
+        
+        if($result){
+            echo "Table Alterd";
+        }else{
+            echo "Table not Alterd.";
+        }
+
+        return $result;
+
+    }
 
 
 }
@@ -80,4 +161,28 @@ $obj = new mySqlDb();
 // $obj->connect_to_db_server();
 $obj-> show_databases();
 // $obj-> create_database("xxx");
+// $qry = "SELECT * FROM user";
+//$qry = " SELECT user_id,post from post WHERE user_id>'2'";
+// $result=$obj->fetch_table_data($qry);
+// echo "<pre>";
+// print_r($result);
+// echo "</pre>";
+// $qry = "INSERT INTO user(name,email) VALUES ('','no2@gmail.com')";
+// $obj->insert_table_data($qry);
+
+// $qry = "UPDATE `user` SET `email` = 'kumkum_2@gmail.com' WHERE `user`.`id` = 6";
+// $obj->update_table_data($qry);
+
+// $qry="ALTER TABLE user MODIFY name varchar(50) NOT NULL";
+// $obj->alter_table($qry);
+
+// $qry="DELETE FROM `user` WHERE `user`.`name` = ''";
+// $obj->delete_table_data($qry);
+
+
+
+
+
 ?>
+
+
--- config.php
+++ config.php
@@ -3,6 +3,6 @@ define("DATABASE","mysql");
 define("SERVER_NAME","localhost:3308");
 define("USER_NAME","root");
 define("PASSWORD","root");
-define("DB_NAME","stocks_data");
+define("DB_NAME","demo");
 
 ?>
